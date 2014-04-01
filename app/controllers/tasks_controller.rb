cad95b3b7a5cffb9b792735028fe298de09950ab
Patti Jurgensen & Angela Yen <student@epicodus.com>
2014-03-31 17:29:42 -0700

    only 7 REST in controller file - working on marked done and undo

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,18 +5,18 @@ class TasksController < ApplicationController
     render('tasks/index.html.erb')
   end
 
-  def done
-    @undone_task = Task.find(params[:id])
-    @undone_task.update(:done => true)
-    render('tasks/done.html.erb')
-  end
-  #Spyder Monkee fixed our done!!!!
+  # def done
+  #   @undone_task = Task.find(params[:id])
+  #   @undone_task.update(:done => true)
+  #   render('tasks/done.html.erb')
+  # end
+  # #Spyder Monkee fixed our done!!!!
 
-  def undo
-    @done_task = Task.find(params[:id])
-    @done_task.update(:done => false)
-    render('tasks/undo.html.erb')
-  end
+  # def undo
+  #   @done_task = Task.find(params[:id])
+  #   @done_task.update(:done => false)
+  #   render('tasks/undo.html.erb')
+  # end
 
   def show
     @task = Task.find(params[:id])
@@ -43,7 +43,8 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
     if @task.update(:description => params[:description],
-                    :due_date => params[:due_date])
+                    :due_date => params[:due_date],
+                    :done => params[:done])
       render('tasks/success.html.erb')
     else
       render('tasks/edit.html.erb')
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 ToDo::Application.routes.draw do
   match('/', {:via => :get, :to => 'tasks#index'})
   match('tasks', {:via => :get, :to => 'tasks#index'})
-  match('tasks/:id/done', {:via => [:patch, :put], :to => 'tasks#done'})
-  match('tasks/:id/undo', {:via => [:patch, :put], :to => 'tasks#undo'})
+  # match('tasks/:id/done', {:via => [:patch, :put], :to => 'tasks#done'})
+  # match('tasks/:id/undo', {:via => [:patch, :put], :to => 'tasks#undo'})
   match('tasks', {:via => :post, :to => 'tasks#create'})
   match('tasks/:id', {:via => :get, :to => 'tasks#show'})
   match('tasks/:id/edit', {:via => :get, :to => 'tasks#edit'})
